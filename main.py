46d66fea7337bd13997ee3f5121a0983027f3dd9
Dhruv Desai <dhruvdesai222@gmail.com>
2023-02-04 11:19:25 +0530

    Added the comment

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-# how to read file in python
+# Printing every line with number
 
 f = open('inputFile.txt','r')
+
+count = 0
+for line in f:
+    print(str(count) + line)
+    count = count + 1
+
 print(f.read())
 f.close()
+
